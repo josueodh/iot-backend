40aeb37f6b77b6cf5627be130468ca2b5c9efd9d
Josué Heringer <josuedelgadoheringer98@gmail.com>
2021-03-02 11:37:24 -0300

    Iniciando backend

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: "52f2f951f5ac0ee29130cc1d1bf803d3",
+    expiresIn: "1d",
+  },
+};
---
+++ src/database/migrations/1614695788415-CreateUser.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export default class CreateUser1614695788415 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
