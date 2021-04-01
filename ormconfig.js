6d0496c3c3f22fbefe726374296c357184286d44
Josué Heringer <josuedelgadoheringer98@gmail.com>
2021-04-01 14:52:35 -0300

    Enviando para o heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,10 @@
+console.log(process.env.DATABASE_URL);
 module.exports = {
   type: "postgres",
   url: process.env.DATABASE_URL,
   entities: ["./dist/models/*.js"],
   migrations: ["./dist/database/migrations/*.js"],
   cli: {
-    migrationsDir: "./dist/database/migrations",
+    migrationsDir: "./src/database/migrations",
   },
 };
